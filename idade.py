c868538ad0073358551e9c5907d3ced5a58dd7bb
eltonpg <eltonpg@hotmail.com>
2016-10-14 23:38:05 -0300

    Add files via upload

---
+++ idade.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton
+
+nome = raw_input("Digite seu nome: ")
+idade = input("Digite a sua idade: ")
+
+#print "Voce e o "+nome+" e a sua idade e "+str(idade)
+print "Voce e o %s e a sua idade e %s"(nome,idade) 
