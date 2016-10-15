1565bd09940607b2d8a7b8294ceb40d9a2a566ae
eltonpg <eltonpg@hotmail.com>
2016-10-14 23:41:11 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton
+
+print "hello Elton"
+
+nome = raw_input("qual a melhor linguagem: ")
+
+#aqui e as validacao dos usuarios
+if nome == "python":
+	print "acertou"
+else:
+	print "errou"
